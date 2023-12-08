509037c7466c1e305fab2710cb8d74f82449acd0
Hayate Fukashiro <hayatefukashiro@mb.local>
2023-12-09 06:11:34 +0900

    スケジュールの新規追加機能を作成

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,11 +1,21 @@
 class SchedulesController < ApplicationController
   def index
+    @schedules = Schedule.all
+    @today = Date.current.strftime("%Y/%m/%d")
   end
 
   def new
+    @schedule = Schedule.new
   end
 
   def create
+    @schedule = Schedule.new(params.require(:schedule).permit(:title, :start_date, :end_date, :all_day, :memo))
+    if @schedule.save
+      flash[:notice] = "スケジュールを登録しました"
+      redirect_to :schedules
+    else
+      render "new"
+    end
   end
 
   def show
