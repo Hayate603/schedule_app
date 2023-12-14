ba574c4ab5148054bb0396c7e60b60afdf27ef04
Hayate Fukashiro <hayatefukashiro@mb.local>
2023-12-14 12:27:53 +0900

    全て空欄で新規作成した際にエラーが発生するのを修正

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -6,7 +6,8 @@ class Schedule < ApplicationRecord
   validate :start_end_check
 
   def start_end_check
-    errors.add(:end_date, "は開始日より前の日付は登録できません。") if
-    self.start_date > self.end_date 
+    if self.start_date.present? && self.end_date.present? && self.start_date > self.end_date
+      errors.add(:end_date, "は開始日より前の日付は登録できません。")
+    end
   end
 end
